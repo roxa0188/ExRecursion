9631d5843db20f6c5fd8cd5cacb22facc82d26d9
Roxana Ion <ionroxana95@yahoo.com>
2017-03-13 09:42:48 +0100

    Add project files.

---
+++ ExRecursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        private void Run()
+        {
+            TowerOfHanoi TOH = new TowerOfHanoi(4);
+            TOH.Solve();
+        }
+
+    }
+}
---
+++ ExRecursion/TowerOfHanoi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExRecursion
+{
+    public class TowerOfHanoi
+    {
+        Stack<int> Tower1 = new Stack<int>();
+        Stack<int> Tower2 = new Stack<int>();
+        Stack<int> Tower3 = new Stack<int>();
+
+        public TowerOfHanoi(int disk)
+        {
+            for (int i = disk; i > 0; i--)
+            {
+                Tower1.Push(i);
+            }
+        }
+
+        public void Solve()
+        {
+            Move(Tower1, Tower2);
+        }
+
+        private bool Move(Stack<int> TowerA, Stack<int> TowerB)
+        {
+            int diskA;
+            int diskB;
+
+            try
+            {
+                diskA = TowerA.Peek();
+            } catch
+            {
+                diskA = 0;
+            }
+
+            try
+            {
+                diskB = TowerB.Peek();
+            }catch
+            {
+                diskB = 0;
+            }
+
+
+            if (diskA < diskB || diskB == 0)
+            {
+                TowerB.Push(TowerA.Pop());
+                return true;
+            }
+            else return false;
+        }
+
+
+
+    }
+}
